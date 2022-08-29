78917ca21508bb9eb49d038a6e95c30ab7e09c1f
Denis Bulyha <felon_91@mail.ru>
2022-08-29 20:32:52 +0300

    task(*): 💡 initial project with codestyle

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// const { GRAPHQL_URL } = require('./config/env');
+
+module.exports = {
+  root: true,
+  extends: [
+    './node_modules/@realtby/codestyle/eslint/prettier',
+    './node_modules/@realtby/codestyle/eslint/base',
+    './node_modules/@realtby/codestyle/eslint/typescript',
+    './node_modules/@realtby/codestyle/eslint/import',
+    './node_modules/@realtby/codestyle/eslint/react',
+    // './node_modules/@realtby/codestyle/eslint/graphql',
+    'plugin:@next/next/recommended',
+  ],
+  rules: {
+    // import
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: ['components', 'lib', 'features'].map(dir => ({
+          pattern: `${dir}/**`,
+          group: 'external',
+          position: 'after',
+        })),
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['./*.{js,ts}', '{pages,pages-archive,@types}/**/*.{ts,tsx}'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+};
