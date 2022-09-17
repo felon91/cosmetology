0de36ac61f38b42215b9fdd972cb60cd2549fa9f
Denis Bulyha <felon_91@mail.ru>
2022-09-17 12:20:41 +0300

    task(*): 💡 add zoom

--- modules/gallery/item.tsx
+++ modules/gallery/item.tsx
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Col, Modal } from 'react-bootstrap';
 import Image from 'next/image';
 
 import { usePopup } from 'lib/hooks/use-popup';
 
 import styles from 'modules/gallery/style.module.css';
+import zoom from 'public/icons/zoom.svg';
 
 interface Props {
   thumbnail: StaticImageData;
@@ -18,18 +19,18 @@ export const GalleryItem: React.FC<Props> = ({ thumbnail, original, quality }) =
   return (
     <>
       <Col sm="6" lg="4">
-        <Image
-          className={styles.pointer}
-          onClick={open}
-          layout="responsive"
-          src={thumbnail}
-          quality={quality}
-        />
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div className={styles.img} onClick={open}>
+          <div className={styles.zoom}>
+            <Image width={40} height={40} src={zoom} quality={quality} alt="Увеличить" />
+          </div>
+          <Image layout="responsive" src={thumbnail} quality={quality} />
+        </div>
       </Col>
       <Modal show={isOpen} onHide={close} size="xl">
         <Modal.Header closeButton />
         <Modal.Body>
-          <Image onClick={open} layout="responsive" src={original} />
+          <Image layout="responsive" src={original} />
         </Modal.Body>
       </Modal>
     </>
