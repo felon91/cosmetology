8257445a63503035a4289dad4679ecebd6c077e4
Denis Bulyha <felon_91@mail.ru>
2022-09-25 16:46:13 +0300

    task(*): 💡 add yandex metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import Script from 'next/script';
+import { Image } from 'react-bootstrap';
 
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
 
@@ -28,6 +30,29 @@ class MyDocument extends Document {
           <meta name="yandex-verification" content="529464850001550b" />
         </Head>
         <body>
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(90546090, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+        });`}
+          </Script>
+          <noscript>
+            <div>
+              <Image
+                src="https://mc.yandex.ru/watch/90546090"
+                style={{ position: 'absolute', left: '-9999px' }}
+                alt=""
+              />
+            </div>
+          </noscript>
           <Main />
           <NextScript />
         </body>
